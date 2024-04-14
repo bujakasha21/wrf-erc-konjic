e104395f51f19f8c92deb6efdf2fd53c8faa28f0
bujakasha21 <bujakdavor21@gmail.com>
2024-04-15 01:50:18 +0200

    changed

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,12 +4,9 @@ import HeroSection from "@/components/home/HeroSection";
 import AboutEvent from "@/components/about/AboutEvent";
 import Organization from "@/components/organization/Organization";
 import Sponsors from "@/components/sponsors/Sponsors";
-import Countries from "@/components/countries/Countries";
 import Gallery from "@/components/gallery/Gallery";
 import Konjic from "@/components/konjic/Konjic";
 import Nerertva from "@/components/neretva/Neretva";
-import Map from "@/components/Map";
-import Title from "@/components/utils/Title";
 import CountriesWrapper from "@/components/countries/CountriesWrapper";
 
 export default function Home() {
