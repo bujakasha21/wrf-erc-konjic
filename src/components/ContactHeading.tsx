f69cd9520782d37fb83e001bb53a605c20f54409
bujakasha21 <bujakdavor21@gmail.com>
2024-04-15 04:00:07 +0200

    almost finished

--- src/components/ContactHeading.tsx
+++ src/components/ContactHeading.tsx
@@ -4,7 +4,7 @@ import { useTranslations } from "next-intl";
 import contact from "@/assets/contactheader.jpg";
 
 const ContactHeading = () => {
-  // const t = useTranslations("Kontakt");
+  const t = useTranslations("Contact");
   return (
     <div className="relative w-full h-screen md:h-[700px]">
       <div
@@ -13,13 +13,13 @@ const ContactHeading = () => {
         className="absolute w-full md:w-2/3 px-4 md:px-28 flex flex-col text-left md:items-start justify-center pt-60"
       >
         <span className="block mb-4 text-base md:text-lg text-thirdCol font-semibold uppercase">
-          {/* {t("contact")} */}fasfas
+          {t("contact")}
         </span>
         <h1 className="text-3xl md:text-5xl font-bold uppercase text-white">
-          {/* {t("header")} */}fsafasf
+          {t("header")}
         </h1>
         <p className="text-base md:text-xl py-5 text-white">
-          {/* {t("description")} */}fsafasf
+          {t("description")}
         </p>
       </div>
       <div className="w-full h-full">
