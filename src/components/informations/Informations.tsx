2d6b8bd9f8b46d647d9a7d78f6c8d1a8598af572
bujakasha21 <bujakdavor21@gmail.com>
2024-06-06 13:39:04 +0200

    files added and fixed

--- src/components/informations/Informations.tsx
+++ src/components/informations/Informations.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import DownloadButton from "@/components/informations/downloadButton";
+import { useTranslations } from "next-intl";
 
 const Informations = () => {
+  const t = useTranslations("Tehnical");
   return (
-    <div className="w-full overflow-hidden px-28 py-20">
+    <div className="w-full overflow-hidden px-6 md:px-28 py-20">
       <div className="flex flex-col w-full items-start">
-        <h3 className="text-4xl font-semibold text-gray-700">Informacije</h3>
+        <h3 className="text-4xl font-semibold text-gray-700">{t("info1")}</h3>
         <hr className="w-full my-8 border-[1px] border-gray-300" />
-        <div className="flex flex-col px-60 my-14 w-full">
+        <div className="flex flex-col px-8 md:px-60 my-14 w-full">
           <DownloadButton
             fileUrl="/download/EventAbout.pdf"
             fileName="Informations About Event"
